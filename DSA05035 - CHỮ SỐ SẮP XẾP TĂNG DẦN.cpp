a61d6a9b89c1d3b03da52bae9f70f7aa480a702f
NGUYỄN NGỌC TOÀN <83264177+NNT1208PT173@users.noreply.github.com>
2022-04-10 06:03:44 +0700

    Create DSA05035 - CHỮ SỐ SẮP XẾP TĂNG DẦN.cpp

---
+++ DSA05035 - CHỮ SỐ SẮP XẾP TĂNG DẦN.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<iomanip>
+#include<string.h>
+#include<climits>
+#include<set>
+#define mod 1000000007
+#define ll long long
+#define	p(x) pair<x,x> 
+#define v(x) vector<x>
+#define FORD(i,l,r) for(int i=l;i<r;i++)
+#define FORE(i,l,r) for(int i=r-1;i>=l;i--)
+#define correct(x,y,n,m) (0<=(x)&&(x)<(n)&&0<=(y)&&(y)<(m))
+#define sz size();
+#define all(M) M.begin(),M.end()
+#define	f first
+#define s second
+#define pb push_back
+#define pf push_font
+using namespace std;
+
+//____________________T_O_A_N_______________________//
+
+
+ll to_Hop(int n, int k) {
+	ll dp[1010][1010];
+	FORD(i, 0, n + 1) {
+		FORD(j, 0, k + 1) {
+			if (i == j || j == 0)dp[i][j] = 1;
+			else dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+			dp[i][j] %= mod;
+		}
+	}
+	return dp[n][k];
+}
+
+int main() {
+	int t; cin >> t;
+	ll dp[1001];
+	dp[1] = 10;
+	FORD(i, 2, 1001)dp[i] = dp[i - 1] % mod + to_Hop(8 + i, i) % mod, dp[i] %= mod;
+	while (t--) {
+		int n; cin >> n;
+		cout << dp[n] << endl;
+	}
+	return 0;
+}
