bdc8f9d75d2613ac0d3ad87fa314256970792fd2
NGUYỄN NGỌC TOÀN <83264177+NNT1208PT173@users.noreply.github.com>
2022-04-24 19:16:24 +0700

    Update and rename DSA02045 - TẬP CON CỦA XÂU KÝ TỰ to DSA02045 - TẬP CON CỦA XÂU KÝ TỰ.cpp

---
+++ DSA02045 - TẬP CON CỦA XÂU KÝ TỰ.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define ll               long long
+#define	p(x)             pair<x,x>
+#define v(x)             vector<x>
+#define tree             node*
+#define sz(a)            a.size()
+#define f                first
+#define s                second
+#define pb(a)            push_back(a)
+#define pf(a)            push_front(a)
+#define FOR(i,l,r)       for(int i=l;i<r;i++)
+#define FORX(i,l,r,x)    for(int i=l;i<r;i+=x)
+#define FORD(i,l,r)      for(int i=l;i>=r;i--)
+#define correct(x,y,n,m) 0<=(x)&&(x)<(n)&&0<=(y)&&(y)<(m)
+#define cin(M,n)         FOR(i,0,n)cin>>M[i]
+#define cout(M,n)        FOR(i,0,n)cout<<M[i]
+#define rs(M,x)          memset(M,x,sizeof(M))
+#define reset()          FOR(i, 0, 1001)A[i].clear(),check[i]=false
+#define faster()         cin.tie(0); ios_base::sync_with_stdio(false); cout.tie(0);
+#define run()            int t; cin >> t; while (t--)
+#define pq(x)            priority_queue<x>
+#define neg_pq(x)        priority_queue<x, vector<x>, greater<x>>
+#define all(M)           M.begin(),M.end()
+using namespace std;
+
+//_______________________NGUYỄN_NGỌC_TOÀN_______________________//
+
+int n;
+string s;
+v(string) res;
+
+void Try(string f, int i) {
+	if(sz(f))
+	res.pb(f);
+	FOR(j, i, n)Try(f + s[j], j+1);
+}
+
+int main() {
+	faster();
+	run() {
+		res.clear();
+		cin >> n >> s;
+		Try("", 0);
+		sort(all(res));
+		for (auto s : res)cout << s << " ";
+		cout << endl;
+	}
+}
+
+//__________________________B20DCPT173__________________________//
