f881dd38917311ef2d21d9a555a291007b96f6d8
NGUYỄN NGỌC TOÀN <83264177+NNT1208PT173@users.noreply.github.com>
2022-06-25 10:39:56 +0700

    Update DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp

--- DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp
+++ DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp
@@ -28,20 +28,18 @@ using namespace std;
 
 int main() {
 	faster();
-	run() {
-		string a, b; cin >> a >> b;
-		int dp[1001][1001];
-		FOR(i, 0, sz(a) + 1) {
-			FOR(j, 0, sz(b) + 1) {
-				if (i == 0 || j == 0)dp[i][j] = 0;
-				else {
-					if (a[i - 1] == b[j - 1])dp[i][j] = dp[i - 1][j - 1] + 1;
-					else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-				}
-			}
-		}
-		cout << dp[sz(a)][sz(b)] << endl;
+	int n; cin >> n;
+	v(int) M(n), dp(n + 1, 1);
+	cin(M, n);
+	int res = 0;
+	FOR(i, 1, n + 1) {
+		int tmp = 1;
+		FOR(j, 0, i)if (M[j] < M[i])tmp = max(tmp, dp[j]);
+		dp[i] += tmp;
+		res = max(res, dp[i]);
 	}
+	cout << res << endl;
+
 }
 
 //__________________________B20DCPT173__________________________//
